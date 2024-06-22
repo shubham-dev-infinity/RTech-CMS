c8c966e98914769ab74d3cced56378ba8b767726
shubham <shubhamsutariya21k@gmail.com>
2024-06-22 17:06:23 +0530

    store configured for add to cart functionality

---
+++ store/cart/index.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+import { ProductQuery } from '../../tina/__generated__/types';
+
+type TProduct = ProductQuery['product']
+interface ICartStore {
+    products: TProduct[];
+    addToCart: (product: TProduct) => void;
+    removeFromCart: (productId: string) => void;
+}
+
+const cartStore = create<ICartStore>()((set, get) => ({
+    products: [],
+    addToCart: (product) => { set({ products: [...get().products, product] }) },
+    removeFromCart: (productId: string) => set({ 'products': get().products.filter((product) => product.uniqueId !== productId) })
+}))
